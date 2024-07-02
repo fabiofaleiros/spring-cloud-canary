fa2d937d90a1e852effaea513b0ace5e52515c70
Fábio Siqueira <fabiofaleiros@gmail.com>
2024-07-02 13:03:58 +0100

    feat(config): simple canary service

---
+++ services/canary-service/src/test/java/tech/ffs/canaryservice/CanaryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.ffs.canaryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanaryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
