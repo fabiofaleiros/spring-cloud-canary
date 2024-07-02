6e91283d9f826ca630ddabd9475ffafcd51e186d
Fábio Siqueira <fabiofaleiros@gmail.com>
2024-07-02 13:04:21 +0100

    feat(config): simple spring cloud gateway

---
+++ services/gateway/src/test/java/tech/ffs/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.ffs.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
