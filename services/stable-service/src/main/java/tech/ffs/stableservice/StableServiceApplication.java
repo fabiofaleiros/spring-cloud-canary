e99c5a9a703fbdbd563256504f42c8937a0d9ee2
Fábio Siqueira <fabiofaleiros@gmail.com>
2024-07-02 13:03:31 +0100

    feat(config): simple stable service

---
+++ services/stable-service/src/main/java/tech/ffs/stableservice/StableServiceApplication.java
@@ -0,0 +1,26 @@
+package tech.ffs.stableservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class StableServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StableServiceApplication.class, args);
+	}
+
+}
+
+@RestController
+class Controller {
+
+	@GetMapping("resource")
+	public String resource(){
+		return "Stable Service";
+	}
+
+
+}
---
+++ services/stable-service/src/test/java/tech/ffs/stableservice/StableServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.ffs.stableservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StableServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
